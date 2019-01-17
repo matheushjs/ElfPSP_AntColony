6be1243c2ecede26fc62ae42b2b02805d19cc26f
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-01-17 16:05:07 -0200

    Add more documentation

--- src/config.h
+++ src/config.h
@@ -7,7 +7,7 @@
  *    configuration parameters as internal variables.
  */
 class Config {
-	static const char *sFilename; /** Name of the configuration file. */
+	static const char *sFilename; /** \brief Name of the configuration file. */
 
 	std::string dHPChain;
 	int         dCycles;
@@ -19,12 +19,25 @@ class Config {
 public:
 	Config();
 
+	/** \brief HP chain of the protein to be predicted. */
 	const std::string&
 	       hp_chain()    const { return dHPChain; }
+
+	/** \brief Number of cycles that the ant colony must perform. */
 	int    cycles()      const { return dCycles; }
+
+	/** \brief Number of ants in the colony. */
 	int    n_ants()      const { return dNAnts; }
+
+	/** \brief Parameter alpha of the ACO probability calculation formula. */
 	double aco_alpha()   const { return dAcoAlpha; }
+
+	/** \brief Parameter beta of the ACO probability calculation formula. */
 	double aco_beta()    const { return dAcoBeta; }
+
+	/** \brief Random seed defined by the user.
+	 *
+	 * If the user-provided seed is negative, that means we should take a random seed, such as `time(NULL).` */
 	int    random_seed() const { return dRandSeed; }
 };
 
