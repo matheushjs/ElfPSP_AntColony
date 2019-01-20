6b16e2a61a14fbfa6857a1c0011936c85e2d380c
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-01-20 20:32:57 -0200

    Add documentation for acopredictor.h

--- src/acopredictor.h
+++ src/acopredictor.h
@@ -7,6 +7,8 @@
 #include "hpchain.h"
 #include "acosolution.h"
 
+/** Encapsulates the whole process of performing Ant Colony Optimization to find
+ *    a protein conformation with low free energy. */
 class ACOPredictor {
 	HPChain dhpchain;
 	int dCycles;
@@ -21,8 +23,19 @@ class ACOPredictor {
 	ACOSolution ant_develop_solution() const;
 
 public:
+	/** Default constructor.
+	 * \param chain HP chain of the protein whose structure is to be predicted.
+	 * \param cycles Number of cycles that should be performed by the ant colony.
+	 * \param nAnts Number of ants in the colony. All ants work in each cycle of the colony.
+	 * \param alpha Parameter alpha of the pheremone probability equation.
+	 * \param beta Parameter beta of the pheremone probability equation.
+	 */
 	ACOPredictor(const HPChain &chain, int cycles, int nAnts, double alpha, double beta);
+
+	/** The destructor frees memory allocated for holding internal data structures. */
 	~ACOPredictor();
 
+	/** Runs the ACO optimization to predict the protein structure.
+	 * \return the best solution found by the optimization algorithm. */
 	ACOSolution predict();
 };
