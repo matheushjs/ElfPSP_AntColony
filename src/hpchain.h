15cc6c9e5570af204719bf40365fae60f5041107
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-01-23 20:44:27 -0200

    Add operator[] to HPChain

--- src/hpchain.h
+++ src/hpchain.h
@@ -4,28 +4,33 @@
 
 #include <string>
 
-/** \brief Encapsulates a string representing an HP chain. */
+/** Encapsulates a string representing an HP chain. */
 class HPChain {
 	std::string dChain;
 
 public:
-	/** \brief Creates an empty HP chain. */
+	/** Creates an empty HP chain. */
 	HPChain()                  : dChain(""){}
 
-	/** \brief Creates an HP chain with the given string representation, which is copied internally. */
+	/** Creates an HP chain with the given string representation, which is copied internally. */
 	HPChain(const char *chain) : dChain(chain){}
 	
-	/** \brief Creates an HP chain with the given string representation, which is copied internally. */
+	/** Creates an HP chain with the given string representation, which is copied internally. */
 	HPChain(std::string chain) : dChain(chain){}
 
-	/** \brief Reference to the internal string. */
+	/** Reference to the internal string. */
 	std::string &get_chain(){ return dChain; }
 
-	/** \brief Length of the chain. */
+	/** Length of the chain. */
 	int length() const { return dChain.length(); }
 
-	/** \brief Validates the internal HP chain.
+	/** Validates the internal HP chain.
 	 *
 	 * Returns 'true' if the internal string contains only 'H' and 'P' characters. */
 	bool validate();
+
+	/** Returns the i-th bead type.
+	 * \return Either 'P' or 'H', the i-th bead's type.
+	 */
+	char operator[](unsigned int i) const { return dChain[i]; }
 };
