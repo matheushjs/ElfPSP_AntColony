dd697f7e369e7b8835a06a98f738eca5eb32e245
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-01-24 00:27:05 -0200

    Now prints on a JSON output file

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,24 @@
 #include <iostream>
+#include <fstream>
 #include <ctime>
 
 #include "hpchain.h"
 #include "config.h"
 #include "vec3.h"
 #include "acopredictor.h"
+#include "acosolution.h"
 
 using namespace std;
+
+inline ostream& operator<<(ostream& stream, const vector<vec3<int>> vec){
+	for(vec3<int> v: vec) stream << v << " ";
+	return stream;
+}
+
+inline ostream& operator<<(ostream& stream, const vector<char> directions){
+	for(char d: directions) stream << ACOSolution::DIR_TO_CHAR(d);
+	return stream;
+}
 
 int main(int argc, char *argv[]){
 	Config conf;
@@ -29,7 +41,12 @@ int main(int argc, char *argv[]){
 
 	ACOSolution result = predictor.predict();
 
-	cout << result << "\n";
+	ofstream outFile = ofstream(conf.filename(), ios::trunc | ios::out);
+	outFile << "{\n";
+	outFile << "\t\"coords\": " << "\"" << result.vector() << "\",\n";
+	outFile << "\t\"hpchain\": " << "\"" << hpchain.get_chain() << "\",\n";
+	outFile << "\t\"directions\": " << "\"" << result.directions() << "\"\n";
+	outFile << "}";
 
 	return 0;
 }
