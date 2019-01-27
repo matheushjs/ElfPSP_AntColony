035f5015cd603a7a1674b60d11fa4629b6680678
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-01-27 01:42:21 -0200

    Add command line argument parsing

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,8 @@
+#include <vector>
 #include <iostream>
 #include <fstream>
 #include <ctime>
+#include <cstdlib>
 
 #include "hpchain.h"
 #include "config.h"
@@ -21,18 +23,55 @@ inline ostream& operator<<(ostream& stream, const vector<char> directions){
 }
 
 int main(int argc, char *argv[]){
+	/* First parse command line arguments. */
+	vector<string> args(argv, argv+argc);
+
+	string argHPChain = "";
+	int argNumCycles  = -1;
+	int argNumAnts    = -1;
+
+	/* Check arguments */
+	for(unsigned i = 1; i < args.size(); i++){
+		if(args[i] == "-h"){
+			cout << "Usage: " << args[0] << " [-h] [-s hp_sequence] [-c num_cycles] [-a num_ants]\n"
+				"\n"
+				"    -h       displays this help message\n"
+				"    -s arg   the HP sequence of the protein to predict\n"
+				"    -c arg   number of cycles that the ant colony should perform\n"
+				"    -a arg   number of ants in the ant colony\n";
+			return 0;
+		} else if(args[i-1] == "-s"){
+			argHPChain = args[i];
+			i++;
+		} else if(args[i-1] == "-c"){
+			argNumCycles = atoi(args[i].c_str());
+			i++;
+			if(argNumCycles <= 0){
+				cerr << "Invalid number of cycles given!\n";
+				exit(1);
+			}
+		} else if(args[i-1] == "-a"){
+			argNumAnts = atoi(args[i].c_str());
+			i++;
+			if(argNumAnts <= 0){
+				cerr << "Invalid number of ants given!\n";
+				exit(2);
+			}
+		}
+	}
+
 	Config conf;
-	HPChain hpchain(conf.hp_chain());
+	HPChain hpchain(argHPChain == "" ? conf.hp_chain() : argHPChain);
 
 	if(!hpchain.validate()){
 		cerr << "Invallid HP Chain. Received: " << hpchain.get_chain() << "\n";
-		std::exit(1);
+		std::exit(2);
 	}
 
 	ACOPredictor predictor(
-		conf.hp_chain(),
-		conf.cycles(),
-		conf.n_ants(),
+		hpchain.get_chain(),
+		argNumCycles == -1 ? conf.cycles()   : argNumCycles,
+		argNumAnts   == -1 ? conf.n_ants()   : argNumAnts,
 		conf.aco_alpha(),
 		conf.aco_beta(),
 		conf.aco_evaporation(),
