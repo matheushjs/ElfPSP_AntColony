10711143db1b49f764280faeadf1c6e0d8aa2c0f
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-01-28 21:59:53 -0200

    Add experiment

---
+++ experiments/1-Jan27-backtrack.bash
@@ -0,0 +1,12 @@
+binary=$1;
+
+declare TIMEFORMAT="%E";
+
+echo "contacts,time";
+
+for hpChain in "PHPHPHHHPPHPHPPPPPPPPPPPHHP HHHHPPPPHPHHPPPHHPPPPPPPPPP PPPPPHHPHPHPHPHPPHHPHHPHPPP"; do
+	for execId in $(seq 100); do
+		output=$( { time ./$binary -s $hpChain | cut -f 2 -d' '; } 2>&1 );
+		echo $output | sed -e "s/ /,/g";
+	done
+done;
