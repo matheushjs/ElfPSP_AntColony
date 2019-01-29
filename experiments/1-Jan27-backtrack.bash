324be734100f72de331cab9b1c4a1251606207a1
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-01-28 22:07:08 -0200

    Fix experiment

--- experiments/1-Jan27-backtrack.bash
+++ experiments/1-Jan27-backtrack.bash
@@ -4,7 +4,7 @@ declare TIMEFORMAT="%E";
 
 echo "contacts,time";
 
-for hpChain in "PHPHPHHHPPHPHPPPPPPPPPPPHHP HHHHPPPPHPHHPPPHHPPPPPPPPPP PPPPPHHPHPHPHPHPPHHPHHPHPPP"; do
+for hpChain in PHPHPHHHPPHPHPPPPPPPPPPPHHP HHHHPPPPHPHHPPPHHPPPPPPPPPP PPPPPHHPHPHPHPHPPHHPHHPHPPP; do
 	for execId in $(seq 100); do
 		output=$( { time ./$binary -s $hpChain | cut -f 2 -d' '; } 2>&1 );
 		echo $output | sed -e "s/ /,/g";
