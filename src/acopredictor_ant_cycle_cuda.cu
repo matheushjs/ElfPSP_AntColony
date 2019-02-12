7f7f895ae7c25ae33fb8a100bcb2880bf3bda6f8
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-02-12 16:55:25 -0200

    Better organize CUDA code

--- src/acopredictor_ant_cycle_cuda.cu
+++ src/acopredictor_ant_cycle_cuda.cu
@@ -52,6 +52,23 @@ int3 operator+(int3 a, int3 b){
 }
 
 __device__
+int norm1(int3 a){
+	return abs(a.x) + abs(a.y) + abs(a.z);
+}
+
+__device__
+void randomize(int &randNumber){
+	randNumber ^= (randNumber << 13);
+	randNumber ^= (randNumber >> 17);
+	randNumber ^= (randNumber << 15);
+}
+
+__device__
+double normal_rand(int randNumber){
+	return (unsigned int) randNumber / (double) UINT_MAX;
+}
+
+__device__
 int3 DIRECTION_VECTOR(int3 prevDir, char dir){
 	if(dir == FRONT){
 		return prevDir;
@@ -85,37 +102,39 @@ int3 DIRECTION_VECTOR(int3 prevDir, char dir){
 }
 
 __device__
-int3 previous_direction(int tid, int3 *solutions, int nCoords, int *progress){
-	// Get pointer to our solution
-	int3 *solution = get_solution(solutions, tid, nCoords);
-	int3 back = solution[progress[tid]+1];
-	int3 backback = solution[progress[tid]];
+int3 previous_direction(int3 *solution, int nCoords, int progress){
+	int3 back = solution[progress+1];
+	int3 backback = solution[progress];
 	return back - backback;
 }
 
 __device__
-int3 solution_back(int tid, int3 *solutions, int nCoords, int *progress){
-	// Get pointer to our solution
-	int3 *solution = get_solution(solutions, tid, nCoords);
-	return solution[progress[tid]+1];
+int3 solution_back(int3 *solution, int nCoords, int progress){
+	return solution[progress+1];
 }
 
-__global__
-void perform_cycle_device(
-	double *pheromone,
-	int nMovElems,
-	int3 *solutions,
-	int nCoords,
-	char *relDirections,
-	int *progress,
-	int3 *possiblePositions
-){
-	int tid = blockIdx.x * blockDim.x + threadIdx.x;
-	int randNumber = (13235632^(threadIdx.x*threadIdx.x+77))>>(threadIdx.x%13); // TODO: Improve RNG
+__device__
+int calculate_contacts(int3 *solution, int nCoords, char *hpChain){
+	int nContacts = 0;
+	for(int i = 0; i < nCoords; i++){
+		if(hpChain[i] == 'P') continue;
+		for(int j = i+1; j < nCoords; j++){
+			if(hpChain[j] == 'H' && norm1(solution[i] - solution[j]) == 1){
+				nContacts++;
+			}
+		}
+	}
+	return nContacts;
+}
 
+__device__
+void develop_solution(int3 *solution, int nCoords, char *myDirections, int nMovElems){
+	int randNumber = (13235632^(threadIdx.x*threadIdx.x+77))>>(threadIdx.x%13); // TODO: Improve RNG
+	int progress = 0;
+	
 	for(int i = 0; i < nMovElems; i++){
-		int3 prevDir = previous_direction(tid, solutions, nCoords, progress);
-		int3 prevBead = solution_back(tid, solutions, nCoords, progress);
+		int3 prevDir = previous_direction(solution, nCoords, progress);
+		int3 prevBead = solution_back(solution, nCoords, progress);
 
 		/*
 		print(prevDir);
@@ -144,35 +163,27 @@ void perform_cycle_device(
 
 		// If all heuristics are 0, there is no possible next direction to take.
 		double sum = heurs[0] + heurs[1] + heurs[2] + heurs[3] + heurs[4];
-		if(sum == 0){
-			int3 *solution = get_solution(solutions, tid, nCoords);
+		if(sum == 0)
 			solution[0].x = -1; // Signalizes error
-		}
 		
 		// TODO: get probabilities
 		double probs[5] = {0.2, 0.2, 0.2, 0.2, 0.2};
 
 		// Accumulate the probability vector
-		for(int i = 1; i < 5; i++)
-			probs[i] += probs[i-1];
+		for(int j = 1; j < 5; j++)
+			probs[j] += probs[j-1];
 
 		// Decide direction
 		char direction = 0;
-		randNumber ^= (randNumber << 13);
-		randNumber ^= (randNumber >> 17);
-		randNumber ^= (randNumber << 15);
+		randomize(randNumber);
 		// Get number within 0-1
-		double unifRand = ((unsigned int) randNumber) / (double) UINT_MAX;
-		for(unsigned i = 0; i < 5; i++){
-			if(unifRand < probs[i]){
-				direction = i;
+		double unifRand = normal_rand(randNumber);
+		for(int j = 0; j < 5; j++){
+			if(unifRand < probs[j]){
+				direction = j;
 				break;
 			}
 		}
-
-		// Add bead and directions, and advance progress
-		int3 *mySolution = get_solution(solutions, tid, nCoords);
-		char *myDirections = get_rel_directions(relDirections, tid, nMovElems);
 
 		/*
 		printf("Decided: %d: ", direction);
@@ -181,10 +192,46 @@ void perform_cycle_device(
 		*/
 
 		// Must be offset by 2, cuz the first 2 are (0,0,0) and (1,0,0) and we disconsider them
-		mySolution[progress[tid]+2] = possiblePos[direction];
-		myDirections[progress[tid]] = direction;
-		progress[tid]++;
+		solution[progress+2] = possiblePos[direction];
+		myDirections[progress] = direction;
+		progress++;
 	}
+}
+
+
+__global__
+void ant_develop_solution_device(
+	double *pheromone,
+	int nMovElems,
+	int3 *solutions,
+	int nCoords,
+	char *relDirections,
+	int3 *possiblePositions,
+	int *contacts,
+	char *hpChain
+){
+	int tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+	// Get pointer to our data
+	int3 *mySolution   = get_solution(solutions, tid, nCoords);
+	char *myDirections = get_rel_directions(relDirections, tid, nMovElems);
+
+	develop_solution(mySolution, nCoords, myDirections, nMovElems);
+
+	// Now we calculate contacts
+	int nContacts = calculate_contacts(mySolution, nCoords, hpChain);
+
+	// Then we perform local search
+
+
+	contacts[tid] = nContacts;
+
+	if(tid == 0){
+		for(int i = 0; i < gridDim.x * blockDim.x; i++){
+			printf("%d: %d\n", i, contacts[i]);
+		}
+	}
+
 
 	/* DEBUG PROTEINS PRODUCED
 	for(int i = 0; i < gridDim.x * blockDim.x; i++){
@@ -233,36 +280,40 @@ void ACOPredictor::perform_cycle(vector<ACOSolution> &antsSolutions, int *nConta
 	 *   - Vectors with relative directions adopted for each solution
 	 *   - Vector for tracking progress for each solution
 	 *   - Vector of 5 possible next positions, for each solution
+	 *   - Vector of contact count, for each solution. We sinalize lost proteins with negative contacts.
+	 *   - Vector of HP chain
 	 * To sinalize error in solutions, we will set the first coordinate to (-1,0,0)
 	 */
 	int nCoords = dNMovElems + 2;
+	string hpChain = dHPChain.get_chain();
 
-	double *d_pheromone; cudaMalloc(&d_pheromone, sizeof(double)*dNMovElems*5);
-	int3 *d_solutions; cudaMalloc(&d_solutions, sizeof(int3)*nCoords*dNAnts);
-	char *d_relDirections; cudaMalloc(&d_relDirections, sizeof(char)*dNAnts*dNMovElems);
-	int *d_progress; cudaMalloc(&d_progress, sizeof(int)*dNAnts);
-	int3 *d_possiblePositions; cudaMalloc(&d_possiblePositions, sizeof(int3)*5*dNAnts);
+	double *d_pheromone;         cudaMalloc(&d_pheromone, sizeof(double)*dNMovElems*5);
+	int3   *d_solutions;         cudaMalloc(&d_solutions, sizeof(int3)*nCoords*dNAnts);
+	char   *d_relDirections;     cudaMalloc(&d_relDirections, sizeof(char)*dNAnts*dNMovElems);
+	int3   *d_possiblePositions; cudaMalloc(&d_possiblePositions, sizeof(int3)*5*dNAnts);
+	int    *d_contacts;          cudaMalloc(&d_contacts, sizeof(int)*dNAnts);
+	char   *d_hpChain;           cudaMalloc(&d_hpChain, sizeof(char)*hpChain.length());
 
 	// Copy stuff
 	cudaMemcpyAsync(d_pheromone, dPheromone, sizeof(double)*dNMovElems*5, cudaMemcpyHostToDevice);
+	cudaMemcpyAsync(d_hpChain, hpChain.c_str(), sizeof(char)*hpChain.length(), cudaMemcpyHostToDevice);
 
 	int3 fillData[2] = {{0,0,0},{1,0,0}};
 	for(int i = 0; i < dNAnts; i++)
 		cudaMemcpyAsync(d_solutions + i*nCoords, fillData, sizeof(int3)*2, cudaMemcpyHostToDevice);
 
-	// Set stuff
-	cudaMemsetAsync(d_progress, 0, sizeof(int)*dNAnts);
-
 	// Call device function
-	perform_cycle_device<<<1,dNAnts>>>(d_pheromone, dNMovElems, d_solutions,
-			nCoords, d_relDirections, d_progress, d_possiblePositions);
+	ant_develop_solution_device<<<1,dNAnts>>>(d_pheromone, dNMovElems, d_solutions,
+			nCoords, d_relDirections, d_possiblePositions, d_contacts,
+			d_hpChain);
 
 	// Free stuff
 	cudaFree(d_pheromone);
 	cudaFree(d_solutions);
 	cudaFree(d_relDirections);
-	cudaFree(d_progress);
 	cudaFree(d_possiblePositions);
+	cudaFree(d_contacts);
+	cudaFree(d_hpChain);
 
 	// Let each ant develop a solution
 	for(int j = 0; j < dNAnts; j++){
