76fd8d0e4d2b44b4d9acdd3e1acad3737f9b651c
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-02-18 23:05:28 -0300

    Finish experiment script

--- experiments/2-Feb18-final.bash
+++ experiments/2-Feb18-final.bash
@@ -1,14 +1,81 @@
 
 TIMEFORMAT="%E";
 
-echo "protSize,wallTime";
+#prog_bt
+#prog_bt_omp
+#prog_nbt
+#prog_nbt_cuda
+#prog_nbt_omp
 
-for protSize in $( seq 64 64 1024 ); do
-	protSequence=$( ./tools/genProt $protSize );
-	
-	for i in $(seq 100); do
-		echo -n "$protSize"",";
-		{ time ./prog_nbt &> /dev/null; } 2>&1 | xargs echo -n;
-		echo;
+PROTSIZE=$(seq 64 64 1024);
+N_ITERATIONS=100;
+
+BINARY=prog_bt
+{
+	echo "protSize,wallTime";
+	for protSize in $PROTSIZE; do
+		protSequence=$( ./tools/genProt $protSize );
+		
+		for i in $(seq $N_ITERATIONS); do
+			echo -n "$protSize"",";
+			{ time ./$BINARY -s $protSequence &> /dev/null; } 2>&1 | xargs echo -n;
+			echo;
+		done
 	done
-done
+} > ${BINARY}_time.out
+
+BINARY=prog_bt_omp
+{
+	echo "protSize,wallTime";
+	for protSize in $PROTSIZE; do
+		protSequence=$( ./tools/genProt $protSize );
+		
+		for i in $(seq $N_ITERATIONS); do
+			echo -n "$protSize"",";
+			{ time ./$BINARY -s $protSequence &> /dev/null; } 2>&1 | xargs echo -n;
+			echo;
+		done
+	done
+} > ${BINARY}_time.out
+
+BINARY=prog_nbt
+{
+	echo "protSize,wallTime";
+	for protSize in $PROTSIZE; do
+		protSequence=$( ./tools/genProt $protSize );
+		
+		for i in $(seq $N_ITERATIONS); do
+			echo -n "$protSize"",";
+			{ time ./$BINARY -s $protSequence &> /dev/null; } 2>&1 | xargs echo -n;
+			echo;
+		done
+	done
+} > ${BINARY}_time.out
+
+BINARY=prog_nbt_cuda
+{
+	echo "protSize,wallTime";
+	for protSize in $PROTSIZE; do
+		protSequence=$( ./tools/genProt $protSize );
+		
+		for i in $(seq $N_ITERATIONS); do
+			echo -n "$protSize"",";
+			{ time ./$BINARY -s $protSequence &> /dev/null; } 2>&1 | xargs echo -n;
+			echo;
+		done
+	done
+} > ${BINARY}_time.out
+
+BINARY=prog_nbt_omp
+{
+	echo "protSize,wallTime";
+	for protSize in $PROTSIZE; do
+		protSequence=$( ./tools/genProt $protSize );
+		
+		for i in $(seq $N_ITERATIONS); do
+			echo -n "$protSize"",";
+			{ time ./$BINARY -s $protSequence &> /dev/null; } 2>&1 | xargs echo -n;
+			echo;
+		done
+	done
+} > ${BINARY}_time.out
